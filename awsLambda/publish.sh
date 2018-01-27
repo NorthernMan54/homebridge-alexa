042962eb6f4845cc9409a42f63d429a666d5dcc2
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-01-27 12:20:51 -0500

    Final

--- awsLambda/publish.sh
+++ awsLambda/publish.sh
@@ -1,7 +1,12 @@
-git add .
-git commit -m "$1"
-git push origin master --tags
+if [ -n "$1" ]
+then
+  echo "Publishing commit $1"
+  #npm version patch
+  git commit -m "$1"
+  git push origin master --tags
+fi
 
 npm install
+echo "ERROR 12 is normal, just means no changes"
 npm run-script package
 npm run-script publish
