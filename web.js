fcaa08df36e1757cb5a4ddac995ebd5a20372029
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-02-12 20:18:28 -0500

    Alexa Event Emitter Complete

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var HttpDispatcher = require('httpdispatcher');
 var dispatcher = new HttpDispatcher();
 var fs = require('fs');
 var path = require('path');
-var debug = require('debug')('Alexa');
+var debug = require('debug')('alexaPlugin');
 
 
 var alexaConnection = require('./lib/alexaLocalClient.js').alexaLocalClient;
@@ -62,13 +62,16 @@ function alexahome(log, config, api) {
   };
 
   hb.discoverHap(log, this.pin);
-  init(this);
-  //var hbAccessories = new hb(this.pin, init());
+//  init(this);
 
-  //    if (api) {
-  //        this.api = api;
-  //        this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
-  //    }
+  alexa = new alexaConnection(options);
+
+  alexa.on('alexa', handleAlexaMessage.bind(this));
+  alexa.on('alexa.discovery', _alexaDiscovery.bind(this));
+  alexa.on('alexa.powercontroller', handleAlexaMessage.bind(this));
+
+
+
 }
 
 alexahome.prototype = {
@@ -85,24 +88,34 @@ alexahome.prototype.configureAccessory = function(accessory) {
   callback();
 }
 
-
-
-function init(self) {
-
-  alexa = new alexaConnection(options);
-
-  alexa.on('alexa',handleAlexaMessage.bind(this));
-
-  alexa.on('alexa.discovery',handleAlexaMessage.bind(this));
-
+function _alexaDiscovery(message, callback) {
+  //debug("handleAlexaMessage", message);
+  var now = new Date();
+  var response = {
+    "event": {
+      "header": {
+        "namespace": "Alexa.Discovery",
+        "name": "Discover.Response",
+        "payloadVersion": "3",
+        "messageId": message.directive.header.messageId
+      },
+      "payload": {
+        "endpoints": hb.endPoints()
+      }
+    }
+  };
+  debug("_alexaDiscovery - returned %s devices", response.event.payload.endpoints.length);
+//  debug("_alexaDiscovery - response", JSON.stringify(response));
+  callback(null, response);
 }
+
 
 function handleAlexaMessage(message, callback) {
   debug("handleAlexaMessage", message);
   var now = new Date();
 
   switch (message.directive.header.namespace.toLowerCase()) {
-    case "alexa":   // aka getStatus
+    case "alexa": // aka getStatus
       var response = {
         "context": {
           "properties": [{
@@ -135,21 +148,6 @@ function handleAlexaMessage(message, callback) {
             "endpointId": message.directive.endpoint.endpointId
           },
           "payload": {}
-        }
-      };
-      break;
-    case "alexa.discovery":
-      var response = {
-        "event": {
-          "header": {
-            "namespace": "Alexa.Discovery",
-            "name": "Discover.Response",
-            "payloadVersion": "3",
-            "messageId": message.directive.header.messageId
-          },
-          "payload": {
-            "endpoints": endPoints()
-          }
         }
       };
       break;
@@ -215,129 +213,6 @@ function handleAlexaMessage(message, callback) {
   debug("handleAlexaMessage - response", JSON.stringify(response));
   callback(null, response);
 }
-
-function endPoints() {
-  var listOfDevices = [];
-  var haps = hb.discover();
-  for (var id in haps) {
-
-    var devices = haps[id];
-
-    for (var did in devices) {
-      var item = {};
-      var device = devices[did];
-      //            console.log("Devices ------------------------------", JSON.stringify(device));
-      item["endpointId"] = new Buffer(device.applianceId).toString('base64');
-      item["friendlyName"] = device.friendlyName;
-      item["description"] = device.friendlyDescription;
-      //      item["applianceId"] = new Buffer(device.applianceId).toString('base64');
-      item["manufacturerName"] = device.manufacturerName;
-      //      item["modelName"] = device.modelName;
-      //      item["version"] = "1.0";
-      item["displayCategories"] = device.displayCategories;
-      item["cookie"] = device.cookie;
-      //      item["isReachable"] = true;
-      item["capabilities"] = device.capabilities;
-      listOfDevices.push(item);
-
-    }
-  }
-  return (listOfDevices);
-}
-
-
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-//dispatcher.setStatic('/static');
-//dispatcher.setStaticDirname(__dirname + "/static");
-
-//A sample GET request
-
-dispatcher.onGet("/ifttt/discover.php", function(req, res) {
-  var listOfDevices = [];
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  var haps = hb.discover();
-  for (var id in haps) {
-
-
-    var devices = haps[id];
-
-    for (var did in devices) {
-      var item = {};
-      var device = devices[did];
-      //            console.log("Devices ------------------------------", JSON.stringify(device));
-      item["applianceId"] = new Buffer(device.applianceId).toString('base64');
-      item["manufacturerName"] = device.manufacturerName;
-      item["modelName"] = device.modelName;
-      item["version"] = "1.0";
-      item["friendlyName"] = device.friendlyName;
-      item["friendlyDescription"] = device.friendlyDescription;
-      item["isReachable"] = true;
-      item["actions"] = device.actions;
-      item["cookie"] = device.cookie;
-      listOfDevices.push(item);
-
-    }
-  }
-  //    console.log("Devices", JSON.stringify(listOfDevices));
-  //    self.log(JSON.stringify(listOfDevices));
-  self.log("Discover request from", req.connection.remoteAddress);
-  self.log("Discover devices returned %s devices", Object.keys(listOfDevices).length)
-  res.end(JSON.stringify(listOfDevices));
-});
-
-dispatcher.onGet("/ifttt/indexd.php", function(req, res) {
-  //    console.log(req);
-
-  var payload = JSON.parse(decodeURI(req.params.device));
-  var action = req.params.action;
-  var applianceId = new Buffer(payload.appliance.applianceId, 'base64').toString().split(":");
-  var characteristics = payload.appliance.cookie[action];
-  var host = applianceId[0];
-  var port = applianceId[1];
-
-  self.log("Control request from", req.connection.remoteAddress);
-  self.log("Control Attempt %s:%s", host, port, action, characteristics);
-
-  switch (action) {
-    case "TurnOffRequest":
-    case "TurnOnRequest":
-      var body = "{ \"characteristics\": [" + characteristics + "] }";
-      break;
-    case "SetPercentageRequest":
-      var t = JSON.parse(characteristics);
-      t.value = payload.percentageState.value;
-      var body = "{ \"characteristics\": [" + JSON.stringify(t) + "] }";
-      break;
-    default:
-      self.log("Unknown Action", action);
-  }
-
-  if (body) {
-    hb.control(host, port, body, function(err, response) {
-
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      self.log("Control Success", response.characteristics);
-      res.end();
-    })
-  } else {
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    self.log("Control Failure");
-    res.end();
-  }
-});
-
-
-dispatcher.onError(function(req, res) {
-  self.log("ERROR-No dispatcher", req.url);
-  res.writeHead(404);
-  res.end();
-});
 
 function alexaResponseSuccess(message) {
   var now = new Date();
