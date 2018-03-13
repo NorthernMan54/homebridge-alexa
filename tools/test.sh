5d2b7f6383680b2abcb39389a8138c3d6d644e83
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-03-12 21:31:06 -0400

    step1

---
+++ tools/test.sh
@@ -0,0 +1,4 @@
+node parseAccessories.js ~/Code/alexaAwsBackend/samples/dyson_accessories.json > after.json ; diff dyson.json after.json
+node parseAccessories.js ~/Code/alexaAwsBackend/samples/penny_accessories.json > after.json ; diff penny.json after.json
+
+
