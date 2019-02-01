2bc4648027e3e0f5912b210400f4d3c8c2df84fd
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-01-31 21:55:19 -0500

    Event Testing

--- tools/parseAccessories.js
+++ tools/parseAccessories.js
@@ -48,7 +48,12 @@ var speakers = [{
 
 var filter = "";
 
-var response = alexaTranslator.endPoints(message, endPoints, { "events": true, "speakers": speakers });
+var response = alexaTranslator.endPoints(message, endPoints, {
+  "events": true,
+  "speakers": speakers
+});
+
+var eventDevices = alexaTranslator.hapEndPoints();
 
 var status = checkAlexaMessage(response);
 if (!status) {
@@ -62,3 +67,36 @@ if (!status) {
 
 console.log("\n-----------------------------------------------------------\n");
 console.log(JSON.stringify(response, null, 4));
+console.log("\n-----------------------------------------------------------\n");
+// console.log(eventDevices);
+
+for (var key in eventDevices) {
+  console.log(key);
+  // console.log(eventDevices[key].endpointID);
+  if (!findById(response, eventDevices[key].endpointID)) {
+    console.log("Fail");
+  }
+  console.log(findById(response, eventDevices[key].endpointID));
+  // console.log(eventDevices.find(eventDevices[key].endpointID));
+}
+
+function findById(o, id) {
+  // Early return
+  // console.log('o.id', o.id);
+  if (o.endpointId === id) {
+    return o;
+  }
+  var result, p;
+  for (p in o) {
+    // console.log(p);
+    if (o.hasOwnProperty(p) && typeof o[p] === 'object') {
+      result = findById(o[p], id);
+      if (result) {
+        return result;
+      }
+    }
+  }
+  return result;
+}
+
+console.log("\n-----------------------------------------------------------\n");
