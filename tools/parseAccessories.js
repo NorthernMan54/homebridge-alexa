34996a76ad8f0f15788487c9196727ef1770cd06
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-02-01 21:54:24 -0500

    Fixed endpoints

--- tools/parseAccessories.js
+++ tools/parseAccessories.js
@@ -9,9 +9,21 @@ var checkAlexaMessage = Validator(alexaSchema, {
 var fs = require('fs');
 
 var endPoints = [{
-  host: "test",
-  port: "12345",
-  HBname: "Testing",
+  ipAddress: "127.0.0.1",
+  instance: {
+    port: 51826,
+    txt: {
+      md: 'parseTest',
+      pv: '1.0',
+      id: 'CC:22:3D:E3:CE:30',
+      'c#': '63',
+      's#': '1',
+      ff: '0',
+      ci: '2',
+      sf: '0',
+      sh: 'kD1sXg=='
+    }
+  },
   accessories: JSON.parse(fs.readFileSync(process.argv[2]).toString())
 }];
 
@@ -45,8 +57,6 @@ var speakers = [{
     "name": "Panasonic"
   }
 ];
-
-var filter = "";
 
 var response = alexaTranslator.endPoints(message, endPoints, {
   "events": true,
