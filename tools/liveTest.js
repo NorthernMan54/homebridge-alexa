1509b73a26bf1b2eef3cc8d96f145ee57a053c81
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-01-14 18:30:32 -0500

    Garage Door and Blinds - Day 1

--- tools/liveTest.js
+++ tools/liveTest.js
@@ -15,7 +15,7 @@ this.log = console.log;
 this.eventBus = new EventEmitter();
 
 this.pin = "031-45-154";
-this.beta = false;
+this.beta = true;
 this.events = false;
 this.oldParser = false;
 this.refresh = 60 * 15; // Value in seconds, default every 15 minute's
@@ -156,7 +156,7 @@ this.eventBus.on('hapEvent', alexaActions.alexaEvent.bind(this));
 this.eventBus.on('System', function(message) {
   this.log.error("ERROR: ", message.directive.header.message);
 }.bind(this));
-this.eventBus.on('Alexa', alexaActions.alexaMessage.bind(this));
+this.eventBus.on('Alexa', alexaMessage.bind(this));
 this.eventBus.on('Alexa.Discovery', alexaDiscovery.bind(this));
 this.eventBus.on('Alexa.PowerController', alexaActions.alexaPowerController.bind(this));
 this.eventBus.on('Alexa.PowerLevelController', alexaActions.alexaPowerLevelController.bind(this));
@@ -168,6 +168,145 @@ this.eventBus.on('Alexa.ThermostatController', alexaActions.alexaThermostatContr
 this.eventBus.on('Alexa.LockController', alexaActions.alexaLockController.bind(this));
 this.eventBus.on('Alexa.ChannelController', alexaActions.alexaChannelController.bind(this));
 this.eventBus.on('Alexa.StepSpeaker', alexaActions.alexaStepSpeaker.bind(this));
+this.eventBus.on('Alexa.ModeController', alexaModeController.bind(this));
+
+/*
+{
+  "directive": {
+    "header": {
+      "namespace": "Alexa.ModeController",
+      "name": "SetMode",
+      "payloadVersion": "3",
+      "instance": "Blinds.Position",
+      "messageId": "53cd9757-8180-40a5-88b8-b52eb9a58cc2",
+      "correlationToken": "AAAAAAAAAAB2FB2EQZWa00PWx2Yaqp8BDAIAAAAAAAD6WXf+ly2plF5NKyZcsTMAVB+Ki/lt9CAhGufdsNU2al6puqgcGWb/wu4JnNDG4j5uxzT7Wr+O54k4qDBY+WwjTz5ejFuq8BkGa1qA8fvMmbwDrcx5bdFMFxU+h/FlRjHcJBdQZ4cGOiZgnjIsK4F+iWA9T8YZ1cDi3+QIAK6rndwflMliZecVI05UB799H3kTdR2ScmMEwFHwtEZ5/T+g4h6dZFCBZWhiTlB7h34vu9FI5l3IOCM4OGlvT4QypThX3VGFDhPCzlqynK7Hu6CZoJj0HgbN5jwFLmyQpnlGvNjcJjxSnR4ubNVy0+mv6xmMxVN6P3wkLgRpmau/lnlFnnjsclYLZdBJQidEUcp6tnGhFbAPaHpjfv9wPTpTKRiSNK5cr1/FYVm0gLpPT9BWyUqblnJC7a0an69uT4jDJuJhPvLqSqFvXyB/Pp0ohOtWtA9ldiNcln0wnPSEYEndhtkhONLmuFXI4uWFUiB1N7h0M5CgBfU0JA987j9rcI3fiCWjGgGtw+mr+L3lp/f99BPZ3Z9G1UD3RwFBnmHEXfnoGZ9uzRvgqlPcJ7ghbq3inXURvSPpj/VxKRA1tINz8ApN1DFPB7jU/wZKgt1whdNQkkI8YLR3LtSUbfwa4J8AX35kQU8rsb8G6r9byD3OTOyGutceEDDk1UDv3sY37X6uBj/Pj6sVD6gZYw=="
+    },
+    "endpoint": {
+      "endpointId": "Q0M6MjI6M0Q6RTM6Q0U6MzAtcGFyc2VUZXN0LUJFVEEtVGVzdCBCbGluZHMtMDAwMDAwOEMtMDAwMC0xMDAwLTgwMDAtMDAyNkJCNzY1Mjkx",
+      "cookie": {
+        "TurnOn": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11,\"value\":100}",
+        "AdjustPowerLevel": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}",
+        "ReportState": "[{\"interface\":\"Alexa.PowerController\",\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11},{\"interface\":\"Alexa.PowerLevelController\",\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}]",
+        "SetPowerLevel": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}",
+        "TurnOff": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11,\"value\":0}"
+      }
+    },
+    "payload": {
+      "mode": "Position.Down"
+    }
+  }
+}
+
+*/
+
+function alexaModeController(message, callback) {
+  var now = new Date();
+  var response = {
+    "event": {
+      "header": {
+        "namespace": "Alexa",
+        "name": "Response",
+        "messageId": message.directive.header.messageId,
+        "correlationToken": message.directive.header.correlationToken,
+        "payloadVersion": "3"
+      },
+      "endpoint": {
+        "endpointId": message.directive.endpoint.endpointId
+      },
+      "payload": {}
+    },
+    "context": {
+      "properties": [{
+        "namespace": "Alexa.ModeController",
+        "instance": message.directive.header.instance,
+        "name": "mode",
+        "value": message.directive.payload.mode,
+        "timeOfSample": now.toISOString(),
+        "uncertaintyInMilliseconds": 500
+      }]
+    }
+  };
+  callback(null, response);
+}
+
+/*
+
+{
+  "directive": {
+    "header": {
+      "namespace": "Alexa",
+      "name": "ReportState",
+      "payloadVersion": "3",
+      "messageId": "5fbdaa33-f283-442a-b9aa-0b42a1e90db9",
+      "correlationToken": "AAAAAAAAAAB2FB2EQZWa00PWx2Yaqp8BDAIAAAAAAAAUcVNoG4N2AfMC3FSjMh/c6k1Mol1QsBXczGwTP9qzDJrn7uKaj0UoLx46PgDgP/4shKpqCYTZ8XGU9Tw+2c2woOplQXojnF3IsDvkkcM56sb73jKVf/WK2lbDRm6cK06wRg66SEQ7qqRIzj9JIAUN3tCZW8t5vvD7bMOH+lzxqL0dik46MmHajNeA/J9hpU9knMcC0wlo9Ozlcs5aynm+lkVLkiLNbzQGi+TYinLA2EbWcO/BHRMDJrKos3INyZ99tWph8rlnsg6H4oRptdNxwpeevweYZnawdky36szMaXTfBjzT55mCjvE+PaBZwPGlHIuk+KBnwZ+b3a9jrCONzMbFwTuwLQ3SeGGpyxw3LmUZSWaWqF704fCRRrfBhhXHKtJk5mCGD8qz4u4DJryQbl6FwxdvNSizn8XvDKf2RNVyHJ1eYkGutrBsn5alnqqoDGdWJC421MJk/jvjDle4I7j0LQ8Xw8tN2qfCLoKQloQBSd8DblP3+9TsAebN/devdetM5wdMnkLb9KbQPsDjGt3LFVWeSgCWAHUTYb4RnL5Leow0tLGYWpk7K9vpENl64GSafqO8lvupALhUCINc7RKQrAWikj+fVwH2gt2ozVQIdNX2b08sQdvTER4lfIIatzxWLsBuWAfdTLvUdp54wP+20u6unq+Ur2+M9u5kPrGEGBSEgwXyImzp7w=="
+    },
+    "endpoint": {
+      "endpointId": "Q0M6MjI6M0Q6RTM6Q0U6MzAtcGFyc2VUZXN0LUJFVEEtRmFrZSBCbGluZHMtMDAwMDAwOEMtMDAwMC0xMDAwLTgwMDAtMDAyNkJCNzY1Mjkx",
+      "cookie": {
+        "TurnOn": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11,\"value\":100}",
+        "AdjustPowerLevel": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}",
+        "ReportState": "[{\"interface\":\"Alexa.PowerController\",\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11},{\"interface\":\"Alexa.PowerLevelController\",\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}]",
+        "SetPowerLevel": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11}",
+        "TurnOff": "{\"host\":\"127.0.0.1\",\"port\":51826,\"aid\":38,\"iid\":11,\"value\":0}"
+      }
+    },
+    "payload": {}
+  }
+}
+
+*/
+
+
+function alexaMessage(message, callback) {
+  var now = new Date();
+  var response = {
+    "event": {
+      "header": {
+        "namespace": "Alexa",
+        "name": "ErrorResponse",
+        "messageId": message.directive.header.messageId,
+        "correlationToken": message.directive.header.correlationToken,
+        "payloadVersion": "3"
+      },
+      "endpoint": {
+        "endpointId": message.directive.endpoint.endpointId
+      },
+      "payload": {
+        "type": "ENDPOINT_UNREACHABLE",
+        "message": "err.message"
+      }
+    }
+  };
+
+  response = {
+    "context": {
+      "properties": [{
+        "namespace": "Alexa.ModeController",
+        "instance": "Blinds.Position",
+        "name": "mode",
+        "value": "Position.Down",
+        "timeOfSample": now.toISOString(),
+        "uncertaintyInMilliseconds": 500
+      }]
+    },
+    "event": {
+      "header": {
+        "messageId": message.directive.header.messageId,
+        "correlationToken": message.directive.header.correlationToken,
+        "namespace": "Alexa",
+        "name": "StateReport",
+        "payloadVersion": "3"
+      },
+      "endpoint": {
+
+        "endpointId": message.directive.endpoint.endpointId,
+        "cookie": {}
+      },
+      "payload": {}
+    }
+  };
+  callback(null, response);
+}
 
 function alexaDiscovery(message, callback) {
   // debug('alexaDiscovery', this);
