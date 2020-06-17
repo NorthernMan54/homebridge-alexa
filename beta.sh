fc45bd474634584d969fc9efc146682f0af90c54
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-06-16 22:39:44 -0400

    fix for #341

---
+++ beta.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+npm audit
+#if npm audit; then
+  npm run-script document
+  rm *orig* *toc\.*
+  git add .
+  npm version patch -m "$1" --force
+  npm publish --tag beta
+  git commit -m "$1"
+  git push origin inputController --tags
+#else
+#  echo "Not publishing due to security vulnerabilites"
+#fi
