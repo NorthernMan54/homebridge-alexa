0af4ad205325f47173d723dc1fd1636aed983f26
Northern Man <northern.man1@gmail.com>
2021-08-30 12:15:22 -0400

    Adjust keepalive to 5 minutes

--- plugin.js
+++ plugin.js
@@ -103,7 +103,7 @@ alexaHome.prototype.didFinishLaunching = function() {
       port: 1883
     }],
     reconnectPeriod: 65000,
-    keepalive: 600,      // Reduce client timeout to 10 minutes
+    keepalive: 300,      // Reduce client timeout to 10 minutes
     // HAP Node Client options
     pin: this.pin,
     refresh: this.refresh,
