7bdf6d72d26cc71de228e6e6cb233249f772c74d
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2024-12-14 14:17:45 -0500

    WIP

--- src/lib/alexaLocal.js
+++ src/lib/alexaLocal.js
@@ -1,198 +1,191 @@
-// Local event based client for alexa
-//
-// Generates events for each Alexa Skill message
-//
-
 "use strict";
 
-var mqtt = require('mqtt');
-var debug = require('debug')('alexaLocal');
+const mqtt = require('mqtt');
+const debug = require('debug')('alexaLocal');
+const Bottleneck = require('bottleneck');
+const Validator = require('is-my-json-valid');
 const packageConfig = require('../../package.json');
-var Bottleneck = require("bottleneck");
+const alexaSchema = require('./alexa_smart_home_message_schema.json');
 
-var Validator = require('is-my-json-valid');
-var alexaSchema = require('./alexa_smart_home_message_schema.json');
-var checkAlexaMessage = Validator(alexaSchema, {
-  verbose: true
-});
+const checkAlexaMessage = Validator(alexaSchema, { verbose: true });
 
-var connection = {};
-var count = 0;
-var username;
-var limiter;
+let connection = {};
+let count = 0;
+let username;
+let limiter;
 let Characteristic;
 
 module.exports = {
-  alexaLocal: alexaLocal,
-  alexaEvent: alexaEvent,
-  alexaPriorityEvent: alexaPriorityEvent
+  alexaLocal,
+  alexaEvent,
+  alexaPriorityEvent,
 };
 
 function alexaLocal(options) {
   debug("Connecting to Homebridge Smart Home Skill");
-  // Throttle event's to match Amazon's Rate API
-  // Limit events to one every 30 seconds, and keep at most 5 minutes worth
+
+  // Configure rate limiting
   limiter = new Bottleneck({
     maxConcurrent: 1,
     highWater: 10,
     minTime: 10000,
-    strategy: Bottleneck.strategy.BLOCK
+    strategy: Bottleneck.strategy.BLOCK,
   });
 
   if (options.alexaService) {
     Characteristic = options.Characteristic;
-    options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
+    options.alexaService.setCharacteristic(
+      Characteristic.ContactSensorState,
+      Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
+    );
   }
 
-  limiter.on("dropped", function (dropped) {
-    options.log("WARNING: ( homebridge-alexa) Dropped event message, message rate too high.");
+  limiter.on("dropped", () => {
+    options.log("WARNING: (homebridge-alexa) Dropped event message, message rate too high.");
   });
 
   username = options.mqttOptions.username;
   connection.client = mqtt.connect(options.mqttURL, options.mqttOptions);
-  connection.client.on('connect', function () {
-    debug('connect', options.mqttURL, "command/" + username + "/#");
-    if (options.alexaService) {
-      options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_DETECTED);
-    }
-    connection.client.removeAllListeners('message'); // Clean up event handlers
-    connection.client.subscribe("command/" + username + "/#");
-    connection.client.publish("presence/" + username + "/1", JSON.stringify({
-      Connected: username,
-      version: packageConfig.version,
-      transport: options.transport,
-      keepalive: options.mqttOptions.keepalive,
-      routines: options.events
-    }));
-    connection.client.on('message', function (topic, message) {
-      try {
-        var msg = JSON.parse(message.toString());
 
-        //    debug("Count", this.listenerCount(msg.directive.header.namespace.toLowerCase()));
-        if (options.eventBus.listenerCount(msg.directive.header.namespace) > 0) {
-          debug('Emitting', msg.directive.header.namespace, msg.directive.header.name, msg.directive.header.correlationToken, msg.directive.endpoint?.endpointId);
-          options.eventBus.emit(msg.directive.header.namespace, msg, function (err, response) {
-            // TODO: if no message, return error Response
-            if (response == null) {
-              response = _alexaErrorResponse(msg);
-              this.log.error("ERROR Response", JSON.stringify(msg));
-            }
-            // debug("Response", JSON.stringify(response));
-            connection.client.publish("response/" + username + "/1", JSON.stringify(response));
-          });
-        } else {
-          debug('No listener for', msg.directive.header.namespace.toLowerCase());
-          var response = _alexaErrorResponse(msg);
-          connection.client.publish("response/" + username + "/1", JSON.stringify(response));
-          debug("Response", JSON.stringify(response, null, 2));
+  connection.client.on('connect', () => handleConnect(options));
+  connection.client.on('offline', () => handleOffline(options));
+  connection.client.on('reconnect', () => handleReconnect(options));
+  connection.client.on('error', (err) => handleError(err, options));
+}
+
+function handleConnect(options) {
+  debug('Connected to', options.mqttURL);
+
+  if (options.alexaService) {
+    options.alexaService.setCharacteristic(
+      Characteristic.ContactSensorState,
+      Characteristic.ContactSensorState.CONTACT_DETECTED
+    );
+  }
+
+  connection.client.removeAllListeners('message');
+  connection.client.subscribe(`command/${username}/#`);
+
+  connection.client.publish(`presence/${username}/1`, JSON.stringify({
+    Connected: username,
+    version: packageConfig.version,
+    transport: options.transport,
+    keepalive: options.mqttOptions.keepalive,
+    routines: options.events,
+  }));
+
+  connection.client.on('message', (topic, message) => handleMessage(topic, message, options));
+}
+
+function handleMessage(topic, message, options) {
+  try {
+    const msg = JSON.parse(message.toString());
+    const namespace = msg.directive.header.namespace;
+
+    if (options.eventBus.listenerCount(namespace) > 0) {
+      debug('Emitting', msg.directive.header.namespace, msg.directive.header.name, msg.directive.header.correlationToken, msg.directive.endpoint?.endpointId);
+
+      options.eventBus.emit(namespace, msg, (err, response) => {
+        if (!response) {
+          response = createAlexaErrorResponse(msg);
+          options.log.error("ERROR Response", JSON.stringify(msg));
         }
-      } catch (err) {
-        options.log.error("ERROR: MQTT Message on topic \"%s\" triggered an internal error\n", topic, err);
-      }
-    });
-  });
-
-  connection.client.on('offline', function () {
-    debug('offline');
-    if (options.alexaService) {
-      options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
-    }
-  });
-
-  connection.client.on('reconnect', function () {
-    count++;
-    debug('reconnect');
-    if (options.alexaService) {
-      options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
-    }
-    if (count % 5 === 0) options.log("ERROR: ( homebridge-alexa) You have an issue with your installation, please review the README.");
-    if (count > 1000) {
-      connection.client.end({
-        force: true
+        connection.client.publish(`response/${username}/1`, JSON.stringify(response));
       });
-      if (options.alexaService) {
-        options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
-      }
-      options.log("ERROR: ( homebridge-alexa) Stopping Home Skill connection due to excessive reconnects, please review the README.");
+    } else {
+      debug('No listener for', namespace);
+      const response = createAlexaErrorResponse(msg);
+      connection.client.publish(`response/${username}/1`, JSON.stringify(response));
     }
-  });
+  } catch (err) {
+    options.log.error(`ERROR: MQTT Message on topic "${topic}" triggered an internal error`, err);
+  }
+}
 
-  connection.client.on('error', function (err) {
-    console.log('ERROR:', err.message, err.code, err)
-    if (options.alexaService) {
-      options.alexaService.setCharacteristic(Characteristic.ContactSensorState, Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
-    }
-    switch (err.code) {
-      case 5:
-        if (process.uptime() < 20) { // If uptime is less than 20 seconds then error is a bad password and not a backend issue
-          options.log.error("ERROR: ( homebridge-alexa) Login to homebridge.ca failed, please validate your credentials in config.json and restart homebridge.");
-          connection.client.end({
-            force: true
-          });
-        } else {
-          options.log.error("ERROR: ( homebridge-alexa) Login to homebridge.ca failed, please validate your configuration in config.json and restart homebridge.");
-        }
-        break;
-      case 'ECONNREFUSED':
-        options.log.error("ERROR: Cloud service DDOS Protection tripped, please stop homebridge for 5 to 10 minutes and try again.");
-        break;
-      default:
-        debug('ERROR: %s -> %d\n', err.message, err.code, err);
-    }
-  });
+function handleOffline(options) {
+  debug('Client offline');
+
+  if (options.alexaService) {
+    options.alexaService.setCharacteristic(
+      Characteristic.ContactSensorState,
+      Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
+    );
+  }
+}
+
+function handleReconnect(options) {
+  count++;
+  debug('Client reconnecting');
+
+  if (options.alexaService) {
+    options.alexaService.setCharacteristic(
+      Characteristic.ContactSensorState,
+      Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
+    );
+  }
+
+  if (count % 5 === 0) {
+    options.log("ERROR: (homebridge-alexa) Review the README for troubleshooting.");
+  }
+
+  if (count > 1000) {
+    connection.client.end({ force: true });
+    options.log("ERROR: Stopping connection due to excessive reconnects.");
+  }
+}
+
+function handleError(err, options) {
+  console.error('ERROR:', err.message, err.code);
+
+  if (options.alexaService) {
+    options.alexaService.setCharacteristic(
+      Characteristic.ContactSensorState,
+      Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
+    );
+  }
+
+  switch (err.code) {
+    case 5:
+      const errorMessage = process.uptime() < 20
+        ? "Login failed. Validate credentials in config.json."
+        : "Login failed. Validate configuration in config.json.";
+      options.log.error(`ERROR: (homebridge-alexa) ${errorMessage}`);
+      connection.client.end({ force: true });
+      break;
+    case 'ECONNREFUSED':
+      options.log.error("ERROR: Cloud service DDOS Protection triggered. Stop Homebridge for 5-10 minutes and try again.");
+      break;
+    default:
+      debug('Unhandled error:', err);
+  }
 }
 
 function alexaEvent(message) {
-  /*
-  var status = checkAlexaMessage(message.toString());
-  if (!status) {
-    debug("WARNING - Bad response message", JSON.stringify(checkAlexaMessage.errors));
-    debug("---------------------------- Request --------------------------------");
-    debug(JSON.stringify(message));
-  }
-  */
-  var topic = "event/" + username + "/1";
-  var publish = function (callback) {
-    debug("Sending message", topic, JSON.stringify(message));
-    connection.client.publish(topic, JSON.stringify(message), {
-      retain: false
-    }, callback);
-  };
-  limiter.submit(publish);
-  // debug("publish limit", limiter.counts());
+  publishMessage(`event/${username}/1`, message);
 }
 
 function alexaPriorityEvent(message) {
-  var topic = "event/" + username + "/1";
-  var publish = function (callback) {
-    debug("Sending priority message", topic, JSON.stringify(message));
-    connection.client.publish(topic, JSON.stringify(message), {
-      retain: false
-    }, callback);
-  };
-  limiter.submit({ priority: 4 }, publish);
-
-  /*
-  var topic = "event/" + username + "/1";
-  debug("Sending priority message", JSON.stringify(message));
-  connection.client.publish(topic, JSON.stringify(message));
-  */
+  limiter.submit({ priority: 4 }, publishMessage, `event/${username}/1`, message);
 }
 
-function _alexaErrorResponse(message) {
-  var response = {
-    "event": {
-      "header": {
-        "name": "ErrorResponse",
-        "namespace": "Alexa",
-        "payloadVersion": "3",
-        "messageId": message.directive.header.messageId
+function publishMessage(topic, message, callback) {
+  debug("Publishing message", topic, JSON.stringify(message));
+  connection.client.publish(topic, JSON.stringify(message), { retain: false }, callback);
+}
+
+function createAlexaErrorResponse(message) {
+  return {
+    event: {
+      header: {
+        name: "ErrorResponse",
+        namespace: "Alexa",
+        payloadVersion: "3",
+        messageId: message.directive.header.messageId,
       },
-      "payload": {
-        "type": "INVALID_DIRECTIVE",
-        "message": "No listener for directive"
-      }
-    }
+      payload: {
+        type: "INVALID_DIRECTIVE",
+        message: "No listener for directive",
+      },
+    },
   };
-  return (response);
 }
